d8093f1267113cd0492616a818e77bec94691ca8
Daviti Vashakmadze <vashaymadzedavit@gmail.comgit config --global user.name Datuchaaa>
2023-08-02 22:36:18 +0400

    feat:improove option

--- src/Components/home/Home.tsx
+++ src/Components/home/Home.tsx
@@ -4,6 +4,11 @@ import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
+import Button from "@mui/material/Button";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import Badge from "@mui/material/Badge";
+import Avatar from "@mui/material/Avatar";
 
 const Home: React.FC = () => {
   return (
@@ -15,6 +20,16 @@ const Home: React.FC = () => {
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
           My App
         </Typography>
+        <IconButton color="inherit">
+          <Badge badgeContent={2} color="error">
+            <NotificationsIcon />
+          </Badge>
+        </IconButton>
+        <IconButton color="inherit">
+          <Avatar>
+            <AccountCircleIcon />
+          </Avatar>
+        </IconButton>
       </Toolbar>
     </AppBar>
   );
